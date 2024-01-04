c3b70b1f20991625bcca185ea1ee5b1e11eef646
Gharsnull <jbecerra627@unab.edu.co>
2024-01-03 19:18:15 -0500

    disable user-select on icon

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -14,6 +14,6 @@ export const appConfig: ApplicationConfig = {
     importProvidersFrom(provideAuth(() => getAuth())),
     importProvidersFrom(provideAnalytics(() => getAnalytics())), ScreenTrackingService, UserTrackingService,
     importProvidersFrom(provideFirestore(() => getFirestore())),
-    importProvidersFrom()
+    importProvidersFrom(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"todo-list-8a8b0","appId":"1:5233612788:web:548aa83db4df8b94898ba1","storageBucket":"todo-list-8a8b0.appspot.com","apiKey":"AIzaSyAt7-oJdVOE56myPztyR7J8KcC8QcBKqeQ","authDomain":"todo-list-8a8b0.firebaseapp.com","messagingSenderId":"5233612788","measurementId":"G-RFX33L981S"}))), importProvidersFrom(provideAnalytics(() => getAnalytics())), ScreenTrackingService, importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"todo-list-8a8b0","appId":"1:5233612788:web:548aa83db4df8b94898ba1","storageBucket":"todo-list-8a8b0.appspot.com","apiKey":"AIzaSyAt7-oJdVOE56myPztyR7J8KcC8QcBKqeQ","authDomain":"todo-list-8a8b0.firebaseapp.com","messagingSenderId":"5233612788","measurementId":"G-RFX33L981S"}))), importProvidersFrom(provideAnalytics(() => getAnalytics())), ScreenTrackingService, importProvidersFrom(provideFirestore(() => getFirestore()))
   ]
 };
